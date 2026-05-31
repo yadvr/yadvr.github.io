ed4cd0663aeb9cff063c5cd19c28f363b6061eb4
Rohit Yadav <rohit@scaleninja.com>
2026-06-01 03:30:42 +0530

    adopt archives as posts index
    Signed-off-by: Rohit Yadav <rohit@scaleninja.com>

--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,9 @@ export const SITE = {
   title: "Rohit Yadav",
   ogImage: "avatar.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 5,
-  postPerPage: 5,
+  postPerIndex: 10,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-  showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
     enabled: true,
